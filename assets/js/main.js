d8bf5107264795a4454b3c0f24bf41ecb18993b2
MrBurp <andreass.ziegler@gmail.com>
2019-01-16 11:14:49 +0100

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,7 @@ Write a function named 'calculateAge' that takes 2 argument:
 - an animal
 - the age of the animal
 
-calculates the corrispondent human age of your animal based on the conversion rates below and outputs the result to the screen like so: "Your dog is XX years old in human years (YY dog years)!" or "Your cow is XX years old in human years (YY cow years)!" or "Your chinchilla is NN years old in human years (YY chinchilla years)!"
+calculates the correspondent human age of your animal based on the conversion rates below and outputs the result to the screen like so: "Your dog is XX years old in human years (YY dog years)!" or "Your cow is XX years old in human years (YY cow years)!" or "Your chinchilla is NN years old in human years (YY chinchilla years)!"
 
 Conversion rates: 
 - 1 year for a dog is 7 years of human life.
@@ -17,7 +17,7 @@ Call the function three times with different sets of values.
 Hints, try first with one animal first and then use conditionals for other animals!
 Use literals
 
-Bonus: use DOM manipulation and create a mini-webapp! 
+Bonus: use DOM manipulation and create a mini-web app! 
 Check https://github.com/FBW-12/fortune-js 
 HTML/CSS/JS with animal images from (https://unsplash.com/)
 
